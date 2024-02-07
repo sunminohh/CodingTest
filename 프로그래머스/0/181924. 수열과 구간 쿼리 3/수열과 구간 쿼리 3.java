7ef6f9f7ec6c27c9a0505ce382c4372d3eb08e1f
sunminohh <130140763+sunminohh@users.noreply.github.com>
2024-02-07 21:30:24 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.05 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
@@ -1,11 +1,12 @@
 class Solution {
     public int[] solution(int[] arr, int[][] queries) {
         
-        int n = 0;
-        for (int i=0; i<queries.length; i++) {
-            n = arr[queries[i][0]];
-            arr[queries[i][0]] = arr[queries[i][1]];
-            arr[queries[i][1]] = n;
+        for(int[] query : queries) {
+            int i = query[0];
+            int j = query[1];
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp; 
         }
     
        return arr;
