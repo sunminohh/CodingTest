a5a1d7da1dcb6e275cc70b6ee251db29e1e1c26d
sunminohh <130140763+sunminohh@users.noreply.github.com>
2024-02-07 18:59:08 +0900

    [level 0] Title: 수 조작하기 1, Time: 1.05 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
@@ -3,18 +3,10 @@ class Solution {
         
         for (int i=0; i<control.length(); i++) {
             switch(control.charAt(i)) {
-                case 'w':
-                    n += 1;
-                    break;
-                case 's':
-                    n -= 1;
-                    break;
-                case 'd':
-                    n += 10;
-                    break;
-                case 'a':
-                    n -= 10;
-                    break;
+                case 'w': n += 1; break;
+                case 's': n -= 1; break;
+                case 'd': n += 10; break;
+                case 'a': n -= 10; break;
             }
         }
         
