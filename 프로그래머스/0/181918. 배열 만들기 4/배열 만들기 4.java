4adf0e0f8ed5f0d2ac91a4fab25e69625a4d489d
sunminohh <130140763+sunminohh@users.noreply.github.com>
2024-02-08 20:33:50 +0900

    [level 0] Title: 배열 만들기 4, Time: 3.58 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        ArrayList<Integer> stkList = new ArrayList<>();
+        int i = 0;
+        
+        while (i < arr.length) {
+            if (stkList.isEmpty()) {
+                stkList.add(arr[i]);
+                i++;
+            } else if (stkList.get(stkList.size() - 1) <arr[i]) {
+                stkList.add(arr[i]);
+                i++;
+            } else if (stkList.get(stkList.size() -1) >= arr[i]) {
+                stkList.remove(stkList.size() -1);
+            }
+        }
+        
+        int[] stk = new int[stkList.size()];
+        for (int j=0; j<stkList.size(); j++) {
+            stk[j] = stkList.get(j);
+        }
+        
+        return stk;
+    }
+}
