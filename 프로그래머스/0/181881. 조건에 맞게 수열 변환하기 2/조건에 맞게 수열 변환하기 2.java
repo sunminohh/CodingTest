5eee1bb20b5445b6e22515b8a75720a829602039
오민선 <130140763+sunminohh@users.noreply.github.com>
2024-03-25 19:25:44 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 2, Time: 0.02 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int solution(int[] arr) {
+        
+        int answer = 0;
+        int count = 0;
+        int[] newArr = new int[arr.length];
+        
+        while (count != arr.length) {
+            count = 0;
+            newArr = arr.clone();
+            
+            for (int i=0; i<arr.length; i++) {
+                if (arr[i]%2==0 && 50<arr[i]) {
+                    arr[i] /= 2;
+                } else if (arr[i]%2!=0 && arr[i]<50) {
+                    arr[i] = arr[i]*2 + 1;
+                }
+                
+                if (arr[i] == newArr[i]) {
+                    count++;
+                }
+            }
+            answer++;
+        }
+        
+        return answer-1;
+    }
+}
