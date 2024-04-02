a32efd142a356efe80f93af7220d6592ef439574
오민선 <130140763+sunminohh@users.noreply.github.com>
2024-04-02 20:59:56 +0900

    [level 0] Title: 커피 심부름, Time: 0.04 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(String[] order) {
+        
+        int answer = 0;
+        
+        for(String str : order) {
+            if (str.contains("cafelatte")) {
+                answer += 5000;
+            } else {
+                answer += 4500;
+            }
+        }
+        
+        return answer;
+    }
+}
