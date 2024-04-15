cc23693c1e6d46bfcaf53fad355addbf0087af18
오민선 <130140763+sunminohh@users.noreply.github.com>
2024-04-15 22:30:44 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 1.67 ms, Memory: 70 MB -BaekjoonHub

---
+++ 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.Collections;
+
+class Solution {
+    public String solution(String s) {
+        
+        String answer = "";
+        
+        String[] strArr = s.split("");
+        Arrays.sort(strArr, Collections.reverseOrder());
+        
+        for (String str : strArr) {
+            answer += str;
+        }
+        
+        return answer;
+    }
+}
